ba8c5abad613d0ef7ca9c79f2c7115da19c714b1
jonhal <simotophs@gmail.com>
2016-11-24 13:59:18 +0800

    Update getModule.py

--- getModule.py
+++ getModule.py
@@ -14,11 +14,13 @@ sys.setdefaultencoding('utf8')
 
 DIR_NAME = MODULE_NAME = sys.argv[1]
 modulesInAll = [MODULE_NAME]
+modulesDepends = []
 FINISHEDFILE = "pythonFinished.txt"
 PREPATH = "./component_src/"
 MODULEPATH = "./" + MODULE_NAME + '/' + MODULE_NAME + '/'
+TMPDEPENDPATH = "depends"
 #进入工作目录
-def readyForWork():
+def readyForWork(): 
     os.chdir(PREPATH)
     if os.path.exists(FINISHEDFILE):
         os.remove(FINISHEDFILE)
@@ -30,6 +32,7 @@ def readyForWork():
 
 #获取git代码
 def getModule(module_name):
+    oldModulesDepends = modulesDepends
     REMOTE_URL = "https://github.com/finance-sh/" + module_name + ".git"
     moduleDownLoadPath = module_name
     if os.path.isdir(moduleDownLoadPath):
@@ -39,13 +42,19 @@ def getModule(module_name):
     origin = repo.create_remote('origin' , REMOTE_URL)
     origin.fetch()
     origin.pull(origin.refs[0].remote_head)
-
+    readJson(module_name)
     print "------getModule " + module_name + " Done--------"
 
 #模块安装
 def moduleInstall(module_name):
     os.system("cd " + module_name + "&& npm install")
     print "------module install end------"
+
+#依赖模块文件位置处理
+def moveDependModulePath(module_name):
+    for startdir in modulesDepends:
+        shutil.move(startdir + '/' + startdir, MODULE_NAME)
+        shutil.rmtree(startdir)
 
 #模块压缩
 def zipModule(module_name):
@@ -85,15 +94,30 @@ def mergeJson(module_name):
 
 #读取本地依赖模块
 def readJson(module_name):
+    oldModulesDepends = modulesDepends
     jsonFilePath = file ("./" + module_name + '/package.json')
     packJson = json.load(jsonFilePath)
     path = packJson['ownModuleDownLoadPath']
-    print path 
+    print path
     print "path"
     for moduleName in packJson['ownModuleDependencies']:
-        modulesInAll.append(path + moduleName)
-        getModule(moduleName)
-        shutil.move(moduleName, path + moduleName)
+        #暂时不用配置文件ownModuleDownLoadPath
+        #newModuleDependPath = path + moduleName
+        newModuleDependPath = moduleName
+        print "==modulesDepends==="
+        print modulesDepends
+        print "==newModuleDependPath==="
+        print newModuleDependPath
+        if newModuleDependPath not in modulesDepends:
+            modulesInAll.append(newModuleDependPath)
+            modulesDepends.append(newModuleDependPath)
+            getModule(newModuleDependPath)
+            # shutil.move(module_name, newModuleDependPath)
+
+#下载依赖文件
+def loadDependsFilesAll(module_name):
+    getModule(MODULE_NAME)
+
 
 #结束脚本通知
 def iAmEnd():
@@ -103,12 +127,14 @@ def iAmEnd():
     f.close()
 
 def main():
+    FIRSTTIMEFLAG = 1
+    print FIRSTTIMEFLAG
     readyForWork()
-    getModule(MODULE_NAME)
-    readJson(MODULE_NAME)
+    loadDependsFilesAll(MODULE_NAME)
+    mergeJson(MODULE_NAME)
+    moveDependModulePath(MODULE_NAME)
     # moduleInstall(MODULE_NAME)
     zipModule(MODULE_NAME)
-    mergeJson(MODULE_NAME)
     iAmEnd()
 
 main()
